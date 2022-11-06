999e5211536b55b9c58d9eb3aaeec2555242128f
Ismail Magdy <92678464+ismailmagdyismail@users.noreply.github.com>
2022-11-06 16:01:05 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,10 +8,5 @@ int main() {
     BankApplication bankSystem;
     bankSystem.run();
 
-   // SavingAccount account("100",100);
-
-    //Client c("ISMAIL","CAIRO","01157077022",&account);
-
-    //    c.displayInfo();
     return 0;
 }
